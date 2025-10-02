3acbd5911e84dfe21d83600318473e5d39f28c01
ayushhkrr <ayushhkrr@gmail.com>
2025-10-02 12:55:27 +0530

    setup completed for the project

---
+++ app.js
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import User from "./model/users.js";
+dotenv.config();
+app.use(express.json());
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const connectdb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Succesfully connected to MongoDB");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+const startServer = async () => {
+  await connectdb();
+  app.listen(PORT, () => {
+    console.log(`The server is running on PORT ${PORT}`);
+  });
+};
+startServer();
