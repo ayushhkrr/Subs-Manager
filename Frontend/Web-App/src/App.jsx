47906660627a85fc12a2387d48cd7dbd8e96b6f2
ayushhkrr <ayushhkrr@gmail.com>
2025-10-12 23:51:02 +0530

    form logic added

--- Frontend/Web-App/src/App.jsx
+++ Frontend/Web-App/src/App.jsx
@@ -1,2 +1,15 @@
 import React from 'react'
 
+const FormPage = ()=>{
+    return <div>
+        <form>
+            <label htmlFor="loginIdentifier">Email or Username</label>
+            <input type="text" placeholder='Enter your email or Username' id='loginIdentifier'/>
+            <br />
+            <label htmlFor="password">Password</label>
+            <input type="password" placeholder='Enter your password' id='password' />
+            <br />
+            <button type='submit'>Log In</button>
+        </form>
+    </div>
+}
