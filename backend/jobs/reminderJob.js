4c712537407c9513ae8552697010bf535b8c3821
ayushhkrr <ayushhkrr@gmail.com>
2025-11-10 12:41:19 +0530

    fixed the directory issue

--- backend/jobs/reminderJob.js
+++ backend/jobs/reminderJob.js
@@ -1,5 +1,5 @@
 import cron from "node-cron";
-import { Subscription } from "../model/allSchemas.js";
+import { Subscription } from "../backend/model/allSchemas.js";
 
 const subsChecker = async () => {
   try {
